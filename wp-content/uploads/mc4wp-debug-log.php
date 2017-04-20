c3e3c45258207d2c565bcea226b9c9af5230f98e
world <world@host.serkankoc.net>
2017-04-20 19:45:58 +0300

    ll

--- wp-content/uploads/mc4wp-debug-log.php
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -1,2 +1,4 @@
 <?php exit; ?>
 [2017-03-30 19:23:34] ERROR: Form 3313 > MailChimp API error: 404 Not Found. The requested resource could not be found.
+[2017-04-01 19:16:28] ERROR: Form 3313 > MailChimp API error: 404 Not Found. The requested resource could not be found.
+[2017-04-01 19:16:46] ERROR: Form 3313 > MailChimp API error: 404 Not Found. The requested resource could not be found.
